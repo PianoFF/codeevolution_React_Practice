b6bc873782cf92c7817bde77f16cc3307b2717a5
PianoFF <Fangfei1star@gmail.com>
2020-01-25 16:21:11 +0000

    state & setState()

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import { Greet } from './components/Greet'
 import Welcome from './components/Welcome'
+import Message from './components/Message'
+
+
 
 function App() {
   return (
     <div className="App">
-      <Greet name='John' heroName='Superman'>
+      <Message /> 
+      {/* <Greet name='John' heroName='Superman'>
         <p> This is a children element </p> 
       </Greet>
-      <Welcome name="Bruce" heroName="Batman"/>
+      <Welcome name="Bruce" heroName="Batman"/> */}
     </div>
   );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Welcome extends Component {
-
-    render(){
-        const { name, heroName } = this.props 
-        return(
-            <div>
-                <h1> Class Component</h1>  
-                <h2> Welcome {name}, a.k.a. {heroName}</h2>
-            </div>
-        ) 
-    }
+  render() {
+    const { name, heroName } = this.props;
+    return (
+      <div>
+        <h1> Class Component</h1>
+        <h2>
+          {" "}
+          Welcome {name}, a.k.a. {heroName}
+        </h2>
+      </div>
+    );
+  }
 }
 
-export default Welcome 
+export default Welcome;
